276c0d0a725eaf399b308763304da64410116adb
kviatkovskii <sergejkvyatkovskij@gmail.com>
2019-12-29 20:20:37 +0300

    fix

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ VALUE=${1?Error: no number given}
 carthage build --no-skip-current
 echo "Build for Carthage!"
 
+git add .
 git commit -m "update version to $VALUE"
 git tag $VALUE
 git push origin $VALUE
