e63e5ca1aebe00dbddfb6de0617c84438f9e560b
kviatkovskii <sergejkvyatkovskij@gmail.com>
2019-12-29 20:16:39 +0300

    update script

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 VALUE=${1?Error: no number given}
 carthage build --no-skip-current
+echo "Build for Carthage!"
+
 git commit -m "update version to $VALUE"
 git tag $VALUE
 git push origin $VALUE
 pod lib lint
 pod trunk push
+echo "Build for CocoaPods!"
