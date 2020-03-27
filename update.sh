32224b9c7429ee329f5fcafed5cc4065303d7d9b
kviatkovskii <sergejkvyatkovskij@gmail.com>
2020-03-27 21:49:33 +0300

    minor correction update.sh

--- update.sh
+++ update.sh
@@ -2,13 +2,11 @@
 
 VALUE=${1?Error: no number given}
 carthage build --no-skip-current
-echo "Build for Carthage!"
+echo "Created build for Carthage!"
 
 git add .
 git commit -m "update version to $VALUE"
 git push -u origin master
 git tag $VALUE
 git push origin $VALUE
-pod lib lint
 pod trunk push
-echo "Build for CocoaPods!"
