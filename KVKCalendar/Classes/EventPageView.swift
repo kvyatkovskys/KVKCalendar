4a2c6537c2a27e291a8dfb65bcb13169613f5cb0
kviatkovskii <sergejkvyatkovskij@gmail.com>
2020-07-19 14:07:41 +0300

    corrections

--- KVKCalendar/Classes/EventPageView.swift
+++ KVKCalendar/Classes/EventPageView.swift
@@ -7,14 +7,12 @@
 
 import UIKit
 
-private let pointX: CGFloat = 5
-
 final class EventPageView: EventPageViewGeneral {
     let event: Event
     let style: Style
     
-    private let color: UIColor
-    
+    private let pointX: CGFloat = 5
+        
     private let textView: UITextView = {
         let text = UITextView()
         text.backgroundColor = .clear
@@ -35,7 +33,6 @@ final class EventPageView: EventPageViewGeneral {
     init(event: Event, style: Style, frame: CGRect) {
         self.event = event
         self.style = style
-        self.color = EventColor(event.color?.value ?? event.backgroundColor).value
         super.init(style: style, event: event, frame: frame)
         
         backgroundColor = event.backgroundColor
@@ -66,21 +63,6 @@ final class EventPageView: EventPageViewGeneral {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override func draw(_ rect: CGRect) {
-        guard let context = UIGraphicsGetCurrentContext() else { return }
-        context.interpolationQuality = .none
-        context.saveGState()
-        context.setStrokeColor(color.cgColor)
-        context.setLineWidth(2)
-        let x: CGFloat = 1
-        let y: CGFloat = 0
-        context.beginPath()
-        context.move(to: CGPoint(x: x, y: y))
-        context.addLine(to: CGPoint(x: x, y: bounds.height))
-        context.strokePath()
-        context.restoreGState()
-    }
 }
 
 protocol EventPageDelegate: class {
@@ -94,9 +76,11 @@ public class EventPageViewGeneral: UIView {
     weak var delegate: EventPageDelegate?
     
     private let event: Event
+    private let color: UIColor
     
     init(style: Style, event: Event, frame: CGRect) {
         self.event = event
+        self.color = EventColor(event.color?.value ?? event.backgroundColor).value
         super.init(frame: frame)
         
         tag = "\(event.id)".hashValue
@@ -130,5 +114,20 @@ public class EventPageViewGeneral: UIView {
         default:
             break
         }
+    }
+    
+    public override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.interpolationQuality = .none
+        context.saveGState()
+        context.setStrokeColor(color.cgColor)
+        context.setLineWidth(2)
+        let x: CGFloat = 1
+        let y: CGFloat = 0
+        context.beginPath()
+        context.move(to: CGPoint(x: x, y: y))
+        context.addLine(to: CGPoint(x: x, y: bounds.height))
+        context.strokePath()
+        context.restoreGState()
     }
 }
