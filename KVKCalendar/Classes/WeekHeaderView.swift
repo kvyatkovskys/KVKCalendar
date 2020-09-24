dd47a7feda31ee68d8aa056ec5adba666661418c
kviatkovskii <sergejkvyatkovskij@gmail.com>
2020-09-24 18:04:34 +0300

    style corrections

--- KVKCalendar/Classes/WeekHeaderView.swift
+++ KVKCalendar/Classes/WeekHeaderView.swift
@@ -11,9 +11,10 @@ final class WeekHeaderView: UIView {
     private var style: Style
     private let fromYear: Bool
     
-    private let titleLabel: UILabel = {
+    private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.textAlignment = .center
+        label.textAlignment = style.month.titleDateAligment
+        label.font = style.month.fontTitleDate
         label.tag = -999
         return label
     }()
@@ -40,15 +41,25 @@ final class WeekHeaderView: UIView {
             days += leftDays
         }
         
+        if !style.month.isHiddenTitleDate && !fromYear {
+            titleLabel.frame = CGRect(x: 10,
+                                      y: 5,
+                                      width: frame.width - 10,
+                                      height: style.month.heightTitleDate)
+            addSubview(titleLabel)
+        }
+        
+        let y = titleLabel.frame.height
+        let xOffset: CGFloat = fromYear ? 0 : 10
         let width = frame.width / CGFloat(days.count)
         for (idx, value) in days.enumerated() {
-            let label = UILabel(frame: CGRect(x: width * CGFloat(idx),
-                                              y: 0,
-                                              width: width,
+            let label = UILabel(frame: CGRect(x: (width * CGFloat(idx)) - xOffset,
+                                              y: y,
+                                              width: width - xOffset,
                                               height: fromYear ? frame.height : style.month.heightHeaderWeek))
             label.adjustsFontSizeToFitWidth = true
             label.minimumScaleFactor = 0.6
-            label.textAlignment = .center
+            label.textAlignment = fromYear ? style.year.weekDayAligment : style.month.weekDayAligment
             label.font = fromYear ? style.year.weekFont : style.month.weekFont
             
             if value.isWeekend {
@@ -69,13 +80,6 @@ final class WeekHeaderView: UIView {
             }
             label.tag = value.shiftDay
             addSubview(label)
-        }
-        if !style.month.isHiddenTitleDate && !fromYear {
-            titleLabel.frame = CGRect(x: 0,
-                                      y: style.month.heightHeaderWeek,
-                                      width: frame.width,
-                                      height: style.month.heightTitleDate - 10)
-            addSubview(titleLabel)
         }
     }
     
