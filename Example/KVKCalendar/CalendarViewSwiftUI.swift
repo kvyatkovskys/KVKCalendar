7eef072907cb57b1455ebc3938cf3d88400b895e
kviatkovskii <sergejkvyatkovskij@gmail.com>
2020-10-31 23:34:33 +0300

    added a swiftUI class for testing

---
+++ Example/KVKCalendar/CalendarViewSwiftUI.swift
@@ -0,0 +1,25 @@
+//
+//  CalendarViewSwiftUI.swift
+//  KVKCalendar_Example
+//
+//  Created by Sergei Kviatkovskii on 31.10.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+struct CalendarViewSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct CalendarViewSwiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CalendarViewSwiftUI()
+        }
+    }
+}
