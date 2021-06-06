c93211dd9e724d41a9e0c86741e40729b98c76be
kviatkovskii <sergejkvyatkovskij@gmail.com>
2021-06-06 22:45:21 +0300

    bumped version

--- update.sh
+++ update.sh
@@ -4,9 +4,6 @@ VALUE=${1?Error: no number given}
 carthage build --no-skip-current --use-xcframeworks
 echo "Created build for Carthage!"
 
-git add .
-git commit -m "update version to $VALUE"
-git push -u origin master
 git tag $VALUE
 git push origin $VALUE
 pod trunk push
