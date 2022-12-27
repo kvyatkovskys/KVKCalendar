8c338487fc69b83529a57c6ebcf72408fa3e6a75
Kviatkovskii Sergei <sergejkvyatkovskij@gmail.com>
2022-12-27 12:18:39 +0300

    bumped version in the package file

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "KVKCalendar",
     defaultLocalization: "en",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
